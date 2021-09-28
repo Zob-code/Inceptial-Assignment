5ec527ef8e52790f4d19112e5c3ae2932f440c25
Zobaier Rahaman <zobaier007@gmail.com>
2021-09-29 01:14:44 +0530

    Printing, Printing

---
+++ Ex8.py
@@ -0,0 +1,12 @@
+formater = "{} {} {} {}"
+
+print(formater.format(1,2,3,4))
+print(formater.format("One","Two","Three","four"))
+print(formater.format(True, False, True, False))
+print(formater.format(formater,formater,formater,formater))
+print(formater.format(
+    "Try Your",
+    "Own Text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
