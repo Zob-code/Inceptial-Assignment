a71857d059b9b13b3dd7e556a3ce3d16105b9a33
Zobaier Rahaman <zobaier007@gmail.com>
2021-09-29 00:28:13 +0530

    Comments and Pound Characters

---
+++ Ex2.py
@@ -0,0 +1,9 @@
+# A comment so you can read your code later.
+# anything after the # is ignored by python.
+
+print("I could have code like this") # and the comment is ignored.
+
+# You can also write comment to disable or comment out a code:
+# print("This won't run")
+
+print("this will run")
