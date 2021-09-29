cb5834ac290e57d0a70d69922d0eec81c5f23a49
Zobaier Rahaman <zobaier007@gmail.com>
2021-09-29 12:04:14 +0530

    Parameter, Unpacking, Variables

---
+++ Ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+#read the WYSS section fot how to run this
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("The First variable is called:", first)
+print("The Second variable is called:", second)
+print("The Third variable is called:", third)
+
+# you have to run this file in the terminal in this way
+
+#  python Ex13.py Zobaier Rahaman good
